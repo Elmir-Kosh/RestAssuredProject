f6ba5b4400dfb9bb9ffd4804a7b47c4b36f47033
Elmir Koshlubaev <elmir-kosh@mail.ru>
2022-01-02 12:21:00 +0500

    refactor

--- src/test/java/ApiBlackJackTest.java
+++ src/test/java/ApiBlackJackTest.java
@@ -7,51 +7,51 @@ import java.io.IOException;
 import static io.restassured.RestAssured.given;
 
 
-public class APIBlackJackTest {
+public class ApiBlackJackTest {
 
-    String URLAdress = "https://qa-demo.kdev2.cone.ee/";
+    String urlAdress = "https://qa-demo.kdev2.cone.ee/";
     String x_api_key = new pages.Util().getPropertyValue("P_KEY");
     String gameName = "SuperGame";
-    String URLforNewGame = "https://qa-demo.kdev2.cone.ee/api/game?gameName=" + gameName + "&gameVersion=";
+    String urlforNewGame = "https://qa-demo.kdev2.cone.ee/api/game?gameName=" + gameName + "&gameVersion=";
     int gameVersion = 2;
 
 
         @Test
-        void checkMainPage() throws IOException {
+        public void checkMainPage() throws IOException {
             given()
-                    .get(URLAdress)
+                    .get(urlAdress)
                     .then()
                     .statusCode(200);
         }
 
         @Test
-    void keyCheck() throws IOException {
+    public void keyCheck() throws IOException {
 
             given()
                     .when ()
                     .contentType (ContentType.JSON)
-                    .get (URLAdress+"key-check?apiKey="+ x_api_key)
+                    .get (urlAdress +"key-check?apiKey="+ x_api_key)
                     .then()
                     .statusCode(200).log().all();
         }
 
         @Test
-    void createNewGameTest() throws IOException {
+    public void createNewGameTest() throws IOException {
 
             given()
                     .when()
                     .contentType(ContentType.JSON)
                     .header("x-api-key", x_api_key)
-                    .post(URLforNewGame + gameVersion)
+                    .post(urlforNewGame + gameVersion)
                     .then()
                     .statusCode(200).log().all();
         }
 
             @Test
-        void getGameInfoTest (){
-                String json = given().when().header("x-api-key", x_api_key).post(URLforNewGame + gameVersion).asString();
-                String ID = JsonPath.read(json, "$..id").toString();
-                String id = ID.substring(2, ID.length() - 2);
+        public void getGameInfoTest (){
+                String json = given().when().header("x-api-key", x_api_key).post(urlforNewGame + gameVersion).asString();
+                String idFull = JsonPath.read(json, "$..id").toString();
+                String id = idFull.substring(2, idFull.length() - 2);
                 System.out.println("Game id: " + id);
 
                 given()
@@ -64,8 +64,8 @@ public class APIBlackJackTest {
         }
 
         @Test
-    void giveCardTest() {
-            String json = given().when().header("x-api-key", x_api_key).post(URLforNewGame + gameVersion).asString();
+    public void giveCardTest() {
+            String json = given().when().header("x-api-key", x_api_key).post(urlforNewGame + gameVersion).asString();
             String ID = JsonPath.read(json, "$..id").toString();
             String id = ID.substring(2, ID.length() - 2);
             System.out.println("Game id: " + id);
